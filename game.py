c432c63f84cc8c0649c031e4bee4430d4b9ce2df
LukeStump <74683887+LukeStump@users.noreply.github.com>
2024-12-02 11:18:33 -0500

    added scoring for words

--- game.py
+++ game.py
@@ -2,8 +2,26 @@ class Game:
     def __init__(self, board):
         self.board = board
 
-    def score(word):
-        """ returns the score of a word based off of its length and rarity
-            input word: String
-        """
-        pass
+def score(word):
+    """ returns the score of a word based off of its length and rarity
+        input word: String
+        note that this does not check that it is on the game board
+    """
+    length = len(word)
+    mit = occurs(word.lower(),"mitDictionary.txt")
+    scrabble = occurs(word.upper(), "scrabbleDictionary.txt")
+
+    if not (mit or scrabble):
+        return None
+    
+    # only mit is 2x, only scrabble is 1x, both is 3x
+    multiplier = 2*mit + scrabble
+
+    return length*multiplier
+
+def occurs(word, fileName):
+    file = open(fileName, "r")
+    for line in file:
+        if word == line.strip():
+            return True
+        print(score(word))
