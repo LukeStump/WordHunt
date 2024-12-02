aec2d3d12370aeee8b37a9f282e059cb87a15f86
angelwith <alekhang@clarku.edu>
2024-12-02 11:15:23 -0500

    Update leaderboard.py

--- leaderboard.py
+++ leaderboard.py
@@ -5,8 +5,10 @@ output = open("leaderboard.txt","w")
 #psuedocode
 output.write(game_seed + player_name + score + "\n")
 
+#^^ Move this into a different file maybe
+d = {} #dictionary
+
 "more psuedocode"
 for line in output: #get each line from leaderboard.txt
     data = line.split() #split the data of each line
     if data[0] == (current_seed) #if the seed in the line matches the seed of the current game
-        #add it to a dictionary which will then be sorted
