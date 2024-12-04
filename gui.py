00b7a59a890e78e9e7404f8798e91d913ba0f60a
angelwith <alekhang@clarku.edu>
2024-12-04 11:03:04 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -6,4 +6,10 @@ root = tk.Tk()
 root.geometry("600x600") #window size
 root.title("Word Hunt")
 
+#Score text
+score_dis = tk.Label(root, text = "Score: XXXX", font=('Arial',20))
+score_dis.pack(padx=20, pady=20)
+
+#create a custom-sized square grid and insert the generated seed into the grid
+
 root.mainloop()
