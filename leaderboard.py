7db085ac04d8b8ed21507261ce763f48eb699ae4
angelwith <alekhang@clarku.edu>
2024-12-05 15:33:25 -0500

    Small edits

--- leaderboard.py
+++ leaderboard.py
@@ -1,21 +1,43 @@
-#write data to leaderboard
+#written on a separate file for now
+import tkinter as tk
+'''GUI.................'''
+root = tk.Tk()
+
+root.geometry("600x600") #window size
+root.title("Leaderboard")
+
+#Score text
+score_dis = tk.Label(root, text = "Leaderboard", font=('Arial',20))
+score_dis.pack(padx=20, pady=20)
+
+#create a custom-sized square grid and insert the generated seed into the grid
+
+root.mainloop()
+'''GUI end'''
+
+
+#write game data to leaderboard
 #probably won't be it's own python file
 output = open("leaderboard.txt","w")
 
 #psuedocode
-output.write(game_seed + player_name + score + "\n")
+#output.write(game_seed + player_name + score + "\n")
 
 #^^ Move this into a different file maybe
 
-d = {} #dictionary for specific game_seed, 
+
+
 #key = player_name, value = score
 
 "more psuedocode"
-"Create a leaderboard for the respective game_seed from the data of leaderboard.txt"
-for line in output: #get each line from leaderboard.txt
-    data = line.split() #split the data of each line
-    if data[0] == (current_seed): #if the seed in the line matches the seed of the current game
-        d += data[1] #initialize key
-        data[1] = score #set key value?
-
-    #print all the scores down, 
+"store scores of specific seed into a dictionary"
+def seed_leaderboard(current_seed):
+    d = {} #dictionary for specific game_seed, 
+    for line in output: #get each line from leaderboard.txt
+        data = line.split() #split the data of each line
+        if data[0] == (current_seed): #if the seed in the line matches the seed of the current game
+            d += data[1] #initialize key (player)
+            data[1] = data[2] #set key value (score)
+    print(d)
+        #print all the scores in the window
+seed_leaderboard("test_seed")
