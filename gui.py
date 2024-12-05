e1efa3ecdfc67f5e889e332b9f4160f81e4d0eb1
angelwith <alekhang@clarku.edu>
2024-12-05 18:20:33 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -6,6 +6,8 @@ root = tk.Tk() #main
 root.geometry("900x600") #window size
 root.title("Word Hunt")
 
+frame = tk.Frame(root, bg="lightblue")
+frame.place(relwidth=1, relheight=1)
 
 #Score text
 score_dis = tk.Label(root, 
@@ -27,16 +29,20 @@ score_dis.place(x=100,y=500)
 # scrollbar1.config( command=mylist.yview)
 
 #Textbox for user input
-input = Text(root, height = 5, width = 40)
+input = Text(root, height = 1, width = 16,font=('Arial',30))
 input.insert(tk.END, "WORRRDDDDD")
-input.place(x=450,y=400)
+input.place(x=480,y=385)
+
+#Button to submit word
+submit = Button(root, height=2,width=21,text="Submit", font=('Arial',20))
+submit.place(x=486,y=450)
 """ create a custom-sized square grid 
 and insert the generated seed into the grid """
 grid_size = 4 #this can be changed
 root.columnconfigure(0, weight=0)
 for i in range(grid_size):
     for j in range(grid_size):
-        frame = tk.Frame(root, bg='red', width=100, height=100)
+        frame = tk.Frame(root, bg='black', width=100, height=100)
         frame.grid(row=i, column=j+1,padx=5,pady=5)
 root.columnconfigure(grid_size+1, weight=1)
 
