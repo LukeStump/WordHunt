870ac621e01d4bc69741491fbd2bc70b065852a1
angelwith <alekhang@clarku.edu>
2024-12-05 23:18:41 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -1,4 +1,6 @@
 #written on a separate file for now
+import board 
+import game
 import tkinter as tk
 from tkinter import *
 root = tk.Tk() #main
@@ -17,9 +19,13 @@ left_frame.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
 right_frame = tk.Frame(master=root, bg="#9fbded")
 right_frame.pack(side=tk.RIGHT, expand=True, fill=tk.BOTH)
 
+#init
+gameBoard = board.makeRandomBoard(4,4, board.generateSeed())
+g = game.Game(gameBoard)
+
 #Timer label
 time_dis = tk.Label(right_frame, 
-text = "Time: X:XX", 
+text = "Time: 0:00", 
 font=('Arial',20),bg='#9fbded',
 anchor=tk.NW)
 time_dis.pack(side=tk.TOP, pady=10, padx=10, anchor=tk.E)
@@ -32,21 +38,20 @@ word_list.pack(side=tk.TOP, expand=True, fill=tk.BOTH, padx=10, pady=5)
 
 scrollbar1 = tk.Scrollbar(right_frame,orient=VERTICAL)
 
-my_list = ["Hello", "Cell", "Fungus","Hello", "Cell", "Fungus",]
+my_list = []
 mylist = Listbox(right_frame, yscrollcommand = scrollbar1.set ) 
 scrollbar1.config( command=mylist.yview)   
 for i in (my_list):
     word_list.insert(END, i)
 
 #Validation message
-submit = Label(right_frame, height=1,text="Error: This is a filler message.", font=('Arial',14),fg="#4f0b12",bg="#9fbded")
+submit = Label(right_frame, height=1,text="", font=('Arial',14),fg="#4f0b12",bg="#9fbded")
 submit.pack(side=tk.TOP, pady=10)
 
 #Textbox for user input
 input = Text(right_frame, height = 1, width = 16,font=('Arial',30))
-input.insert(tk.END, "WORRRDDDDD")
+input.insert(tk.END, "")
 input.pack(side=tk.TOP)
-#input.place(x=480,y=385)
 
 #Button to submit word
 submit = Button(right_frame, height=2,width=21,text="Submit", font=('Arial',20))
@@ -70,12 +75,12 @@ for i in range(grid_size):
     for j in range(grid_size):
         frame = tk.Frame(left_frame, bg='#d6e6ff', width=frame_size, height=frame_size)
         frame.grid(row=i, column=j+1,padx=pad_size,pady=pad_size)
-        label = tk.Label(frame, text='K', font=('Arial',letter_size))
+        letter = gameBoard.getLetter((i,j))
+        label = tk.Label(frame, text=letter, font=('Arial',letter_size))
         label.place(relwidth=1, relheight=1)
 
 #Score text
-score_dis = tk.Label(left_frame, text = "Score: XXXX", font=('Arial',30),bg='#9fbded',anchor=tk.NW)
+score_dis = tk.Label(left_frame, text = "Score: 0", font=('Arial',30),bg='#9fbded',anchor=tk.NW)
 score_dis.grid(row=left_size-1, columnspan=left_size, pady=10)
 #score_dis.place(x=100,y=470)
-
 root.mainloop()
