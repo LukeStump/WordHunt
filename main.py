463874c4c1957580c723efba9576c7b1713397b2
LukeStump <74683887+LukeStump@users.noreply.github.com>
2024-12-05 23:24:49 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,64 @@
 # The Main file of the Game
-import board
+import re
+# from game import Game
 import game
+import board
 
+def commandLine():
+    # when backend does frontend
+    options()
+    print("yipee")
 
-def playTest():
-    gameBoard = board.makeBoard("OATRIHPSHTNRENEI",4,4)
-    g = game.Game(gameBoard)
+def options():
+    opts = [("play random", playRandomGame),("compete",compete),("compete [Host]", competeHost),("solve",solve)]
+    for i in range(len(opts)):
+        print(i+1, opts[i][0])
+    opt = int(input(f"[1-{i+1}]:"))
+    opt = opts[opt-1][1]
+    opt.__call__()
+
+def getGame(boardString = None):
+    if boardString == None:
+        boardString = input("enter board String:")
+    g = None
+    if re.match("\w+ \d+x\d+", boardString):
+        g = game.makeGameFromBoardString(boardString, scoreLimit=1)
+    else:
+        g = game.makeGame(4,4, scoreLimit=1)
+    return g
+
+def playGame(g):
+    try:
+        g.gameLoop()    
+    except game.GameOver:
+        pass
+
+def playRandomGame():
+    g = getGame("")
+    playGame(g)
+
+def competeHost():
+    g = getGame("")
+    print(g.getBoardString())
+    input("press Enter to start")
+    playGame(g)
+
+def compete():
+    g = getGame()
+    print(g.getBoardString())
+    input("press Enter to start")
+    playGame(g)
+
+def solve():
+    g = getGame()
     g.timer.start_time()
-    while(True):
-        v = g.scorePlayerInput()
-        print("--->",v)
+    trie = board.createBoardTrie(g.board)
+    g.score = trie.countTotalWordLength()
+    try:
+        g.gameOver()
+    except game.GameOver:
+        pass
 
-playTest()
+
+if __name__ == "__main__":
+    commandLine()
