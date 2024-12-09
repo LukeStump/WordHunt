5543f180aef9c16c2f3ce80afffaa27f227840ab
angelwith <alekhang@clarku.edu>
2024-12-09 17:20:05 -0500

    Update leaderboard.py

--- leaderboard.py
+++ leaderboard.py
@@ -1,10 +1,8 @@
 #write game data to leaderboard
-#probably won't be it's own python file
 "Variables"
 infile = open("leaderboard.txt","r", encoding='utf-8')
 d = {} #dictionary for specific game_seed : #key = player_name, value = score
 current_seed = "test_seed" #seed of the current game
-
 
 "store scores of specific seed into a dictionary"
 for line in infile: #get each line from leaderboard.txt
@@ -14,8 +12,6 @@ for line in infile: #get each line from leaderboard.txt
     if data[0] == (current_seed):
         d[data[1]] = data[2] #add player and score to dictionary
 
-def get_scores():
-    return d 
 "Testing to see if the code works"
 for i in d:
     print(i, d[i]) #print all the scores in the window
