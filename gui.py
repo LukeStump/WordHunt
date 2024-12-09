0a5670f94fa2c7c6e86dcc6f00de8d0aed29f949
LukeStump <74683887+LukeStump@users.noreply.github.com>
2024-12-08 20:04:54 -0500

    Update gui.py

--- gui.py
+++ gui.py
@@ -14,11 +14,14 @@ y = (screen_height/2) - (h/2)
 root.geometry('%dx%d+%d+%d' % (w, h, x, y)) #window size
 root.title("Word Hunt")
 
-left_frame = tk.Frame(master=root, bg="#9fbded")
-left_frame.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+settings_frame = tk.Frame(master=root, bg="#9fbded")
+settings_frame.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
 
-right_frame = tk.Frame(master=root, bg="#9fbded")
-right_frame.pack(side=tk.RIGHT, expand=True, fill=tk.BOTH)
+board_frame = tk.Frame(master=root, bg="#9fbded")
+board_frame.pack(side=tk.LEFT, expand=True, fill=tk.BOTH)
+
+word_frame = tk.Frame(master=root, bg="#9fbded")
+word_frame.pack(side=tk.RIGHT, expand=True, fill=tk.BOTH)
 
 #init
 grid_size = 4 #this can be changed but additional code needs to be done to
@@ -88,9 +91,17 @@ def updateBoard():
     # TODO
     pass
 
+def setup_board():
+    for i in range(grid_size): #insert letters into grid
+        for j in range(grid_size):
+            frame = tk.Frame(board_frame, bg='#d6e6ff', width=frame_size, height=frame_size)
+            frame.grid(row=i, column=j+1,padx=pad_size,pady=pad_size)
+            letter = gameBoard.getLetter((i,j))
+            label = tk.Label(frame, text=letter, font=('Arial',letter_size))
+            label.place(relwidth=1, relheight=1)
 
 #Timer label
-time_dis = tk.Label(right_frame, 
+time_dis = tk.Label(word_frame, 
 text = "Time: 0:00", 
 font=('Arial',20),bg='#9fbded',
 anchor=tk.NW)
@@ -98,24 +109,24 @@ time_dis.pack(side=tk.TOP, pady=10, padx=10, anchor=tk.E)
 #time_dis.place(x=750,y=30)
 
 #Scrollbar for found words
-word_list = Listbox(right_frame, width=10,font=("Arial",16), justify="center")
+word_list = Listbox(word_frame, width=10,font=("Arial",16), justify="center")
 word_list.pack(side=tk.TOP, expand=True, fill=tk.BOTH, padx=10, pady=5)
 # word_list.place(x=600,y=80)
 
-scrollbar1 = tk.Scrollbar(right_frame,orient=VERTICAL)
+scrollbar1 = tk.Scrollbar(word_frame,orient=VERTICAL)
 
 my_list = []
-mylist = Listbox(right_frame, yscrollcommand = scrollbar1.set ) 
+mylist = Listbox(word_frame, yscrollcommand = scrollbar1.set ) 
 scrollbar1.config( command=mylist.yview)   
 for i in (my_list):
     word_list.insert(END, i)
 
 #Validation message
-vali = tk.Label(right_frame, height=1,text="Waiting for input", font=('Arial',14),fg="#4f0b12",bg="#7792d4")
+vali = tk.Label(word_frame, height=1,text="Waiting for input", font=('Arial',14),fg="#4f0b12",bg="#7792d4")
 vali.pack(side=tk.TOP, pady=10)
 
 #Textbox for user input
-input = Text(right_frame, height = 1, width = 14,font=('Arial',30))
+input = Text(word_frame, height = 1, width = 14,font=('Arial',30))
 input.insert(tk.END, "")
 input.pack(side=tk.TOP)
 
@@ -123,43 +134,42 @@ input.pack(side=tk.TOP)
 and insert the generated seed into the grid """
 left_size = grid_size+2
 for i in range(left_size):
-    left_frame.columnconfigure(i, weight=1)
-    left_frame.rowconfigure(i, weight=1)
+    board_frame.columnconfigure(i, weight=1)
+    board_frame.rowconfigure(i, weight=1)
 frame_size = 420//grid_size
 pad_size = 20//grid_size
 letter_size = 200//grid_size
 
-for i in range(grid_size): #insert letters into grid
-    for j in range(grid_size):
-        frame = tk.Frame(left_frame, bg='#d6e6ff', width=frame_size, height=frame_size)
-        frame.grid(row=i, column=j+1,padx=pad_size,pady=pad_size)
-        letter = gameBoard.getLetter((i,j))
-        label = tk.Label(frame, text=letter, font=('Arial',letter_size))
-        label.place(relwidth=1, relheight=1)
+setup_board()
 
 #Score text
-score_dis = tk.Label(left_frame, text = "Score: 0", font=('Arial',30),bg='#9fbded',anchor=tk.NW)
+score_dis = tk.Label(board_frame, text = "Score: 0", font=('Arial',30),bg='#9fbded',anchor=tk.NW)
 score_dis.grid(row=left_size-1, columnspan=left_size, pady=10)
 score_dis_score = 0
 #score_dis.place(x=100,y=470)
 
+settings_frame.columnconfigure(0, weight=1)
+settings_frame.columnconfigure(3, weight=1)
+
 #Generate seed
-generate = Text(left_frame,height = 1, width = 15,font=('Arial',14),fg='#9a9a9a')
+randomSeed = Button(settings_frame, height=1,width=15,text="Random Seed", font=('Arial',14)) #command=generateSeed) #Generates a random seed
+randomSeed.grid(row=0, columnspan=4, pady=10)
+
+generate = Text(settings_frame,height = 1, width = 15,font=('Arial',14),fg='#9a9a9a')
 generate.insert(tk.END, "Input Seed")
-generate.grid(row=0, column=0, padx=10,pady=1)
+generate.grid(row=1, columnspan=4, pady=10)
 
 #Grid size edit
-gridEdit = Text(left_frame,height = 1, width = 2,font=('Arial',14),fg='#9a9a9a',)
+gridEdit = Text(settings_frame,height = 1, width = 2,font=('Arial',14),fg='#9a9a9a',)
 gridEdit.insert(tk.END, "")
-gridEditText = tk.Label(left_frame, text = "Grid size", font=('Arial',14), bg='#9fbded')
-gridEdit.place(x=100,y=100)
-gridEditText.place(x=10,y=100)
+gridEditText = tk.Label(settings_frame, text = "Grid size:", font=('Arial',14), bg='#9fbded')
+gridEditText.grid(row=2, column=1, pady=10)
+gridEdit.grid(row=2, column=2, pady=10)
 
-randomSeed = Button(left_frame, height=1,width=15,text="Random Seed", font=('Arial',14)) #command=generateSeed) #Generates a random seed
-randomSeed.place(x=7,y=5)
 
-solve = Button(left_frame, height=1,width=15,text="Solve", font=('Arial',14)) #command=generateSeed) #Generates a random seed
-solve.place(x=7,y=150)
+
+solve = Button(settings_frame, height=1,width=15,text="Solve", font=('Arial',14)) #command=generateSeed) #Generates a random seed
+solve.grid(row=3, columnspan=4, pady=10)
 
 g.timer.start_time()
 def wordcheck(word):
@@ -210,7 +220,7 @@ def submitButton(event=None):
 input.bind("<Return>", submitButton) 
 
 #Button to submit word
-submit = Button(right_frame, height=2,width=15,text="Submit", font=('Arial',20),command=submitButton)
+submit = Button(word_frame, height=2,width=15,text="Submit", font=('Arial',20),command=submitButton)
 submit.pack(side=tk.TOP, pady=10)
 #submit.place(x=486,y=450)
 
